bc8fea39347ec2ffbdfa99fb5e8c98c0a1f3d984
Adam Bubnik <halfrozen01@gmail.com>
2021-02-11 15:41:50 +0100

    First commit

---
+++ Hero_and_Dragon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hero_and_Dragon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
