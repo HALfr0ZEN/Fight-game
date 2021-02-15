6014ab23886468ee9e565ca30e65ece8e2152998
Adam Bubnik <halfrozen01@gmail.com>
2021-02-15 10:59:56 +0100

    Character class. Duplicated code remove

---
+++ Hero_and_Dragon/Character.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Hero_and_Dragon
+{
+    class Character
+    {
+        public string Name { get; set; }
+        
+        private int _health;
+
+        public int Health
+        {
+            get => _health;
+            set => _health = value < 0 ? 0 : value; // set to zero if damage is > health
+        }
+
+        public int MaxDamage { get; set; }
+        public int MaxDefense { get; set; }
+
+        private readonly Random _generating = new Random();
+
+        public Character(string name, int health, int maxDamage, int maxDefense)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.MaxDamage = maxDamage;
+            this.MaxDefense = maxDefense;
+        }
+        
+        /*Character attack*/
+        public (int damage, int defense) Attack(Character enemy)
+        {
+            int defense = enemy.Defense();
+
+            int damage = Convert.ToInt32(_generating.NextDouble() * MaxDamage);
+
+            //patch negative numbers 
+            if (damage > defense)
+                enemy.Health -= damage - defense;
+
+            return (damage, defense);
+        }
+
+        /*Random character defense*/
+        public int Defense()
+        {
+            return _generating.NextDouble() <= 0.5 ? _generating.Next(0, MaxDefense) : 0;
+        }
+
+        /*Check if character is alive*/
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+    }
+}
--- Hero_and_Dragon/Characters/Hero.cs
+++ Hero_and_Dragon/Characters/Hero.cs
@@ -2,54 +2,10 @@
 
 namespace Hero_and_Dragon.Characters
 {
-    class Hero
+    class Hero:Character
     {
-        public string Name { get; }
-
-        private int _health;
-
-        public int Health
-        {
-            get => _health;
-            set => _health = value < 0 ? 0 : value; // set to zero if damage is > health
-        }
-
-        public int MaxDamage { get; }
-        public int MaxDefense { get; }
-
-        private readonly Random _generating = new Random();
-
-        public Hero(string name, int health, int maxDamage, int maxDefense)
-        {
-            this.Name = name;
-            this.Health = health;
-            this.MaxDamage = maxDamage;
-            this.MaxDefense = maxDefense;
-        }
-
-        /*Character attack*/
-        public (int damage, int enemyDefense) Attack(Dragon enemy)
-        {
-            int defense = enemy.Defense();
-
-            int damage = Convert.ToInt32(_generating.NextDouble() * MaxDamage);
-
-            //patch negative numbers 
-            if (damage > defense)
-                enemy.Health -= damage - defense;
-
-            return (damage, defense);
-        }
-
-        public int Defense()
-        {
-            return _generating.NextDouble() <= 0.5 ? _generating.Next(0, MaxDefense) : 0;
-        }
-
-        /*Check if character is alive*/
-        public bool IsAlive()
-        {
-            return Health > 0;
-        }
+        /*Calling constructor of the base class (bcs !parameter-less)*/
+        public Hero(string name, int health, int maxDamage, int maxDefense) : base(name,  health, maxDamage,  maxDefense)
+        {}
     }
 }
