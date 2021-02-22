a2a4060b1949199e486509f8daa555218c519210
Adam Bubnik <halfrozen01@gmail.com>
2021-02-22 23:31:45 +0100

    Generalization of all things

---
+++ Hero_and_Dragon/Characters/Character.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Hero_and_Dragon.Characters
+{
+    class Character
+    {
+        public string Name { get; }
+        private int _health;
+
+        public int Health
+        {
+            get => _health;
+            set => _health = value < 0 ? 0 : value; // set to zero if damage is > health
+        }
+
+        private int MaxDamage { get; }
+        private int MaxDefense { get; }
+
+        protected readonly Random generating = new Random();
+
+        protected Character(string name, int health, int maxDamage, int maxDefense)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.MaxDamage = maxDamage;
+            this.MaxDefense = maxDefense;
+        }
+
+        /*
+         *  Character attack
+         */
+        public int Attack(Character enemy)
+        {
+            int defense = enemy.Defense();
+
+            int damage = generating.Next(0,MaxDamage);
+
+            //patch negative numbers 
+            if (damage > defense)
+                enemy.Health -= damage - defense;
+
+            return damage;
+        }
+
+        /*
+         *  Random character defense
+         */
+        private int Defense()
+        {
+            return generating.NextDouble() <= 0.5 ? generating.Next(0, MaxDefense) : 0;
+        }
+
+        /*
+         *  Check if character is alive
+         */
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+
+        /*
+         *  Select opponents from given list
+         *  opponents are objects with different type than called
+         */
+        public Character SelectOpponent(List<Character> characters)
+        {
+            List<Character> opponents = new List<Character>();
+            
+            //Loop thru all players (characters)
+            foreach (var character in characters)
+            {
+                // check if selected opponent is not ally and if its alive
+                // if true add him into opponents list
+                if (character.IsAlive() && character.GetType() != GetType())
+                {
+                    opponents.Add(character);
+                }
+            }
+            
+            // pick one random opponent and return it
+            return opponents.Count > 0 ? opponents[generating.Next(0, opponents.Count)] : null;
+        }
+    }
+}
--- Hero_and_Dragon/Program.cs
+++ Hero_and_Dragon/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
 using Hero_and_Dragon.Characters;
 
 namespace Hero_and_Dragon
@@ -8,43 +10,87 @@ namespace Hero_and_Dragon
         static void Main(string[] args)
         {
             /*fight in loop while one death*/
-            Hero hero = new Hero(name: "Geralt", 500, 35, 50);
-            Dragon dragon = new Dragon(name: "Alduin", 500, 35, 40);
+            Hero hero = new Hero(name: "Geralt", 50, 75, 50);
+            Hero hero2 = new Hero(name: "Dovakhin", 50, 60, 80);
+            Dragon dragon = new Dragon(name: "Alduin", 60, 35, 50);
+            Dragon dragon2 = new Dragon(name: "Šmak", 60, 30, 50);
+            
+            List<Character> characters = new List<Character>();
+            
+            characters.Add(hero);
+            characters.Add(hero2);
+            characters.Add(dragon);
+            characters.Add(dragon2);
 
-
-            for (int i = 1;; i++)
+            for (int i = 1; HeroAliveCount(characters) > 0 && DragonAliveCount(characters) > 0 ; i++)
             {
-                Console.WriteLine(Environment.NewLine + "--------------------");
                 Console.WriteLine("Round: " + i);
-                Console.WriteLine("--------------------");
+                Console.WriteLine("===================");
 
-                (int damage, int enemyDefense) = hero.Attack(dragon);
-
-                Console.WriteLine(hero.Name + " attack: " + damage);
-                Console.WriteLine(dragon.Name + " defend: " + enemyDefense);
-
-                Console.WriteLine(dragon.Name + " health: " + dragon.Health);
-                
-                if (!dragon.IsAlive())
+                int j = 0;
+                foreach (Character attacker in characters)
                 {
-                    Console.WriteLine(Environment.NewLine + hero.Name + " won!");
-                    break;
-                }
-
-                Console.WriteLine();
-                (damage, enemyDefense) = dragon.Attack(hero);
-
-                Console.WriteLine(dragon.Name + " attack: " + damage);
-                Console.WriteLine(hero.Name + " defend: " + enemyDefense);
-
-                Console.WriteLine(hero.Name + " health: " + hero.Health);
-                
-                if (!hero.IsAlive())
-                {
-                    Console.WriteLine(Environment.NewLine + dragon.Name + " won!");
-                    break;
+                    
+                    
+                    if (attacker.IsAlive())
+                    {   
+                        Character opponent = attacker.SelectOpponent(characters);
+                        
+                        if (opponent == null)
+                        {
+                            break;
+                        }
+                        
+                        ++j;
+                        Console.WriteLine("Move: " +j + Environment.NewLine + "-------------------");
+                        
+                        int damage = attacker.Attack(opponent);
+                        Console.WriteLine(attacker.Name + " attack with " + damage);
+                        Console.WriteLine(opponent.Name + " health is now " + opponent.Health);
+                        Console.WriteLine(Environment.NewLine);
+                    }
                 }
             }
+            
+            Console.WriteLine("Winners!"+Environment.NewLine+ "¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤");
+            foreach (var character in characters)
+            {
+                if (character.IsAlive())
+                {
+                    Console.WriteLine(character.Name);
+                }
+            }
+            Console.WriteLine("¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤");
+            
+            //characters.ForEach(Character => Console.WriteLine(Character.IsAlive() ? Character.Name : null));
+            
+        }
+
+        public static int HeroAliveCount(List<Character> characters)
+        {
+            int countAlive = 0;
+            foreach (Character character in characters)
+            {
+                if (character is Hero && character.IsAlive())
+                {
+                        ++countAlive;
+                }
+            }
+
+            return countAlive;
+        }
+        public static int DragonAliveCount(List<Character> characters)
+        {
+            int countAlive = 0;
+            foreach (Character character in characters)
+            {
+                if (character is Dragon && character.IsAlive())
+                {
+                    ++countAlive;
+                }
+            }
+
+            return countAlive;
         }
     }
 }
